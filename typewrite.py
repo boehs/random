9b7f810be685d04d6a844b1d28d9cd7ba119a208
Evan Boehs <ebrobot2000@gmail.com>
2020-09-25 17:52:43 -0400

    tiptap

---
+++ typewrite.py
@@ -0,0 +1,11 @@
+import time
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+def typewrite(text):
+    for char in text:
+        print(char, flush = True, end = "")
+        time.sleep(0.1)
+
+typewrite(input("What to Write: "))
