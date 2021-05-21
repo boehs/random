6ccfee4c4783f6a3b47faf4f21d17b7408413974
Evan Boehs <ebrobot2000@gmail.com>
2021-05-21 01:21:37 -0400

    fmt according to rustfmt

--- rust/trianglearea.rs
+++ rust/trianglearea.rs
@@ -8,7 +8,7 @@
 //! edition = "2018"
 //!
 //! [dependencies]
-//! 
+//!
 //! ```
 // Copyright 2021, Evan Boehs
 
@@ -16,40 +16,34 @@
 use std::io::stdin;
 
 struct Triangle {
-  lineone: f64,
-  linetwo: f64,
-  linethree: f64,
+    lineone: f64,
+    linetwo: f64,
+    linethree: f64,
 }
 
 impl Triangle {
-  fn area(&self) -> f64 {
-    let s = (self.lineone+self.linetwo+self.linethree) / 2.0;
-    (s * (s-self.lineone) * (s-self.linetwo) * (s-self.linethree)).sqrt()
-  }
+    fn area(&self) -> f64 {
+        let s = (self.lineone + self.linetwo + self.linethree) / 2.0;
+        (s * (s - self.lineone) * (s - self.linetwo) * (s - self.linethree)).sqrt()
+    }
 }
 
 fn main() {
-  fn gentri() -> f64 {
-    let mut tri = String::new();
-    stdin().read_line(&mut tri).ok().expect("Failed to read line");
-    tri.trim().parse().unwrap()
-  }
-  let tri = Triangle {
-    lineone: {
-      println!("Enter the length of the first line");
-      gentri()
-    },
-    linetwo: {
-      println!("Enter the length of the second line");
-      gentri()
-    },
-    linethree: {
-      println!("Enter the length of the third line");
-      gentri()
+    fn gentri(n: String) -> f64 {
+        println!("Enter the length of the {} line", n);
+        let mut tri = String::new();
+        stdin()
+            .read_line(&mut tri)
+            .ok()
+            .expect("Failed to read line");
+        tri.trim().parse().unwrap()
     }
-  };
-  println!("The result is {}",tri.area());
-  
+    let tri = Triangle {
+        lineone: { gentri(format!("first")) },
+        linetwo: { gentri(format!("second")) },
+        linethree: { gentri(format!("third")) },
+    };
+    println!("The result is {}", tri.area());
 }
 
 // vim: set ft=rust sw=4 sts=4 expandtab :
