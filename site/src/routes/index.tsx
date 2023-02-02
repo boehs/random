411ba03b91cc9643b3e505d42e6cdbacf2785c25
Evan Boehs <evan@boehs.org>
2023-02-01 21:48:07 -0500

    AOL

--- site/src/routes/index.tsx
+++ site/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { For } from "solid-js";
 import { Title } from "solid-start";
 import EmOMG from "~/components/EmOMG";
 
@@ -7,10 +8,15 @@ export default function Home() {
       <Title>Ahoy!</Title>
       <h1>Hello Hello, Welcome Home!!!</h1>
       <p>
-        I use this as a <EmOMG giphy="XDp6oEZojcCR2L0mlK"/> playground to test <EmOMG kym="000/234/765/b7e"/> various ideas.
+        I use this as a playground to test <EmOMG kym="000/234/765/b7e"/> various ideas.
       </p>
       <img src="https://media.tenor.com/hAlpXvNEQM0AAAAd/bull-dog-dog.gif"/>
       <blockquote>Wherever you go, go with all your heart</blockquote>
+      <div style={{"display": "flex", gap: "5px"}}>
+        <For each={["archlinux",'bvwe','masto','leaveaol','sucks','web3','webpassion']}>
+          {gif => <img src={`/buttons/${gif}.gif`} class="no"/>}
+        </For>
+      </div>
     </main>
   );
 }
