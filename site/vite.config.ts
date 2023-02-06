d0cdf765bfbd25be8be761fa30b4b18f41ecca7a
Evan Boehs <evan@boehs.org>
2023-02-06 09:54:20 -0500

    AAA

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,14 +1,21 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import cloudflare from "solid-start-cloudflare-pages";
-import rucjs from "@rollup/plugin-commonjs";
 
 export default defineConfig({
-  plugins: [rucjs(), solid({adapter: cloudflare({})})],
+  plugins: [solid({adapter: cloudflare({})})],
   ssr: {
     noExternal: ['function-plot','mathjax']
   },
   define: {
     global: "window"
+  },
+  build: {
+    "commonjsOptions": {
+      include: []
+    }
+  },
+  optimizeDeps: {
+    disabled: false
   }
 });
