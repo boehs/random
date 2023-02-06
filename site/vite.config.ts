8ef92628187de9f01da0f63b12d0cb0140e107f3
Evan Boehs <evan@boehs.org>
2023-02-06 09:18:12 -0500

    oh right this thing

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import cloudflare from "solid-start-cloudflare-pages";
 
 export default defineConfig({
-  plugins: [solid({})],
+  plugins: [solid({adapter: cloudflare({})})],
   ssr: {
     noExternal: ['function-plot']
   },
