baa45f25a099db2d811cbbe2d350346acbc98907
Evan Boehs <evan@boehs.org>
2023-02-10 22:25:50 -0500

    remove tf

--- site/src/components/data/Equation.tsx
+++ site/src/components/data/Equation.tsx
@@ -3,17 +3,22 @@ import katex from 'katex'
 import { Link } from "solid-start"
 
 export default function E({ children, type = 'inline' }: { children: string, type: 'inline' | 'block' }) {
+    let str = children
+    try {
+        const tmp = katex.renderToString(children)
+        str = tmp
+    } catch {}
     return <>
         <Link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" />
         <Switch fallback={"e"}>
             <Match when={type == 'inline'}>
-                <span innerHTML={katex.renderToString(children)} />
+                <span innerHTML={str} />
             </Match>
             <Match when={type == 'block'}>
                 <div style={{
                     "font-size": "20px",
                     'text-align': 'center'
-                }} innerHTML={katex.renderToString(children)} />
+                }} innerHTML={str} />
             </Match>
         </Switch>
     </>
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,8 +1,6 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import cloudflare from "solid-start-cloudflare-pages";
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
 
 export default defineConfig({
   plugins: [
@@ -11,8 +9,6 @@ export default defineConfig({
         jsx: true,
         jsxImportSource: "solid-js",
         providerImportSource: "solid-mdx",
-        remarkPlugins: [remarkMath],
-        rehypePlugins: [rehypeKatex]
       }),
       enforce: "pre"
     },
